9412901a6a88f6d2afe00293be6c8040e37f1cb2
LiuRachel <zhengrong_1211@163.com>
2020-02-16 07:34:54 +0800

    Update SeleniumTest.cs

--- test/TechFabricSln.Test/SeleniumTest.cs
+++ test/TechFabricSln.Test/SeleniumTest.cs
@@ -12,10 +12,9 @@ namespace TechFabricSln.Test
         public void VisitMicrosoft_CheckWindowsMenu()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArgument(" --headless");
-            options.AddArgument(" --whitelisted-ips=");
-            options.AddArgument(" --no-sandbox");
-            options.AddArgument(" --disable-extensions");
+            options.AddArgument("--headless");
+            options.AddArgument("/path/to/chromedriver --whitelisted-ips=0.0.0.0");
+            options.AddArgument("--no-sandbox");
             IWebDriver driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://www.microsoft.com/");
 
