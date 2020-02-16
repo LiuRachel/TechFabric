7a0bf1024eb1d8d610f5e66dd2d340c382544d6b
LiuRachel <zhengrong_1211@163.com>
2020-02-16 19:21:06 +0800

    Update SeleniumTest.cs

--- test/TechFabricSln.Test/SeleniumTest.cs
+++ test/TechFabricSln.Test/SeleniumTest.cs
@@ -12,8 +12,7 @@ namespace TechFabricSln.Test
         public void VisitMicrosoft_CheckWindowsMenu()
         {
             var service = ChromeDriverService.CreateDefaultService();
-            service.WhitelistedIPAddresses = "0.0.0.0";
-            service.LogPath = "C:\\chromedriver.log";
+            service.WhitelistedIPAddresses = "";
             service.EnableVerboseLogging = true;
             IWebDriver driver = new ChromeDriver(service);
             driver.Navigate().GoToUrl("https://www.microsoft.com/");
